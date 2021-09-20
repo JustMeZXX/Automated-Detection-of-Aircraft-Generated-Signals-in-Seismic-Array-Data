8833c41ba2cf0993df16f31538d8ae37048798d2
Xinxiang Zhang <xinxiang@smu.edu>
2021-09-19 23:53:32 -0500

    Update sample_resize.m

--- code/data_prep/sample_resize.m
+++ code/data_prep/sample_resize.m
@@ -13,12 +13,11 @@ files = fullfile(path, '*.png'); % can be other formats
 dir = dir(files);
 
 for i = 1:length(dir)
-    
+
     cur_address = [dir(i).folder,'\',dir(i).name];
     cur_img = imread(cur_address);
     
     cur_img_resized = imresize(cur_img,[224,224]);
     
     imwrite(cur_img_resized,dir(i).name) % save as the current name
-    
 end
